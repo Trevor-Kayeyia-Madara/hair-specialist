0bd99b20431353cf07e0211f0283c78f1f852d89
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-02-19 15:51:13 +0300

    Fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
       // Fetch user details from 'users' table
       const { data: userData, error: userError } = await supabase
         .from("users")
-        .select("id, email, password, userType") // Fetch required fields
+        .select("email, password,") // Fetch required fields
         .eq("email", formData.email)
         .single();
   
