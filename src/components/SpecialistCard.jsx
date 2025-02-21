76e2b2c3cb427f799709cb895f84915b61cb672d
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-02-21 12:04:45 +0300

    Fixed

--- src/components/SpecialistCard.jsx
+++ src/components/SpecialistCard.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 const SpecialistCard = ({ specialist }) => {
   const {
     id,
+    full_name,
     speciality,
     service_rates,
     location,
@@ -11,6 +12,7 @@ const SpecialistCard = ({ specialist }) => {
 
   return (
     <div className="specialist-card">
+      <h2>{full_name}</h2>
       <h2>{speciality}</h2>
       <p><strong>Service Rates:</strong> {service_rates}</p>
       <p><strong>Location:</strong> {location}</p>
@@ -24,6 +26,7 @@ const SpecialistCard = ({ specialist }) => {
 SpecialistCard.propTypes = {
   specialist: PropTypes.shape({
     id: PropTypes.number.isRequired,
+    full_name: PropTypes.string.isRequired,
     speciality: PropTypes.string.isRequired,
     service_rates: PropTypes.string,
     location: PropTypes.string,
