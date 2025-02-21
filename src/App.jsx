e2e5d0060a904b87ea274ea4afb499212b8ba1a2
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-02-21 12:20:18 +0300

    HashRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
