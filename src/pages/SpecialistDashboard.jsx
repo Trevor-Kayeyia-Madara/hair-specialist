bbf150d9ce383a94336bd7a1c135f8cb315bbe90
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-02-25 22:19:41 +0300

    Remove ESLint

--- src/pages/SpecialistDashboard.jsx
+++ src/pages/SpecialistDashboard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState, useEffect, useCallback } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { io } from "socket.io-client";
