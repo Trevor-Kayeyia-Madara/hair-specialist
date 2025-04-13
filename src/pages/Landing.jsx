d9d81b91745bdf5519d6ee3a0143fb5f3d1463fb
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-04-13 22:06:36 +0300

    Logged In User at Specialist Card

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -163,6 +163,7 @@ const Landing = () => {
                   key={index}
                   specialist={specialist}
                   startNewChat={startNewChat}
+                  isAuthenticated={loggedIn}
                 />
               ))
             ) : (
