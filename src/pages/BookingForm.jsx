50c7f89a0faac5b6ae2511eaaa61af6411454cf0
Trevor-Kayeyia-Madara <trevormadarakayeyia@gmail.com>
2025-04-14 12:19:20 +0300

    409 fix

--- src/pages/BookingForm.jsx
+++ src/pages/BookingForm.jsx
@@ -111,12 +111,7 @@ const BookingForm = () => {
           status: "Booked",
         }),
       });
-      console.log("Submitting review with:", {
-        userId,
-        specialistId,
-        rating,
-        review,
-      });
+
       const bookingData = await bookingRes.json();
       toast.success("✅ Appointment booked!");
 
